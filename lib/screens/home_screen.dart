3049410aec496affc2a372b0c89d567bdd34e1c9
Alisha <dahalalisha5@gmail.com>
2022-07-19 12:14:33 +0545

    Screen fixes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -25,86 +25,90 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return GestureDetector(
-      onTap: () {
-        if (showDrawer) {
-          setState(() {
-            xOffset = 0;
-            yOffset = 0;
-            scaleFactor = 1;
-            showDrawer = false;
-          });
-        }
-      },
-      child: AnimatedContainer(
-        duration: Duration(
-          milliseconds: 250,
-        ),
-        transform: Matrix4.translationValues(xOffset, yOffset, 0)
-          ..scale(scaleFactor)
-          ..rotateY(showDrawer ? -0.2 : 0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius:
+    return MaterialApp(
+      home: Scaffold(
+        body: GestureDetector(
+          onTap: () {
+            if (showDrawer) {
+              setState(() {
+                xOffset = 0;
+                yOffset = 0;
+                scaleFactor = 1;
+                showDrawer = false;
+              });
+            }
+          },
+          child: AnimatedContainer(
+            duration: Duration(
+              milliseconds: 250,
+            ),
+            transform: Matrix4.translationValues(xOffset, yOffset, 0)
+              ..scale(scaleFactor)
+              ..rotateY(showDrawer ? -0.2 : 0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius:
               showDrawer ? BorderRadius.circular(40) : BorderRadius.circular(0),
-        ),
-        child: SafeArea(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 15,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                        icon: showDrawer
-                            ? Icon(CupertinoIcons.chevron_left)
-                            : Icon(Icons.menu),
-                        onPressed: () {
-                          setState(() {
-                            if (!showDrawer) {
-                              xOffset = size.width * 0.6; //280;
-                              yOffset = size.height / 5;
-                              scaleFactor = 0.6;
-                              showDrawer = true;
-                            } else {
-                              xOffset = 0;
-                              yOffset = 0;
-                              scaleFactor = 1;
-                              showDrawer = false;
-                            }
-                          });
-                        }),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+            child: SafeArea(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          'Canine Care',
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w300,
-                              color: primaryGreen,
-                          ),
-                        ),
+                        IconButton(
+                            icon: showDrawer
+                                ? Icon(CupertinoIcons.chevron_left)
+                                : Icon(Icons.menu),
+                            onPressed: () {
+                              setState(() {
+                                if (!showDrawer) {
+                                  xOffset = size.width * 0.6; //280;
+                                  yOffset = size.height / 5;
+                                  scaleFactor = 0.6;
+                                  showDrawer = true;
+                                } else {
+                                  xOffset = 0;
+                                  yOffset = 0;
+                                  scaleFactor = 1;
+                                  showDrawer = false;
+                                }
+                              });
+                            }),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Canine Care',
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w300,
+                                color: primaryGreen,
+                              ),
+                            ),
 
+                          ],
+                        ),
+                        CircleAvatar(
+                          backgroundImage: AssetImage('images/profile.jpg'),
+                        ),
                       ],
                     ),
-                    CircleAvatar(
-                      backgroundImage: AssetImage('images/profile.jpg'),
-                    ),
-                  ],
-                ),
-              ),
+                  ),
 
-              PetCategoryDisplay(),
-              PetCategories(),
-            ],
+                  PetCategoryDisplay(),
+                  PetCategories(),
+                ],
+              ),
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
